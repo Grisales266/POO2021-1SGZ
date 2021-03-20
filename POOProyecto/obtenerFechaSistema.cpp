f9d500e73ae469a3b53eea6bf12bac7ef054945a
yonyPA59 <yonyp581@gmail.com>
2021-03-19 19:28:41 -0500

    Prueba de integracion

---
+++ POOProyecto/obtenerFechaSistema.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<stdio.h>
+#include<time.h>
+
+const  std::string currentDAteTime(){
+    time_t      now = time(0);
+    struct tm tstruct;
+    char        buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf,sizeof(buf),"%y-%m-%d",&tstruct);
+    return buf;
+
+}
+int main(){
+    std::cout<<"Fecha actual del sistema:"<<currentDAteTime()<<std::endl;
+    getchar();
+}
